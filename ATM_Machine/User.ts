6df3efdf36a6bf4448d51ec6264913af079d1f7c
adeenazara2002 <adinazara2001@gmail.com>
2024-12-09 11:37:39 +0500

    user model

--- ATM_Machine/User.ts
+++ ATM_Machine/User.ts
@@ -1,1 +1,35 @@
-console.log("Hello World");
+export class User {
+    private pin: string;
+    private balance: number;
+
+    constructor(pin: string, balance: number){
+        this.pin = pin;
+        this.balance = balance;
+    }
+
+    validatePin(inputPin: string): boolean{
+        return this.pin == inputPin;
+    }
+
+    getBalance(): number{
+        return this.balance; 
+    }
+
+    // withdrawing money
+
+    withdraw(amount:number):boolean{
+        if (amount>0 && amount<= this.balance) {
+            this.balance -+ amount;
+            return true;
+        }
+        return false;
+    }
+
+    // depositing money
+
+    deposit(amount:number):void{
+        if (amount > 0) {
+            this.balance += amount;
+        }
+    }
+}
